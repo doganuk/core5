471621e31a5b485b554a0a5a84ce93b66f2b85b6
doganuk <dogan.uk@outlook.com>
2022-09-11 12:00:01 +0100

    test5

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Logging;
 namespace Catalog.Controllers
 {
 
-    
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
